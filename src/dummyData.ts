a63acaf26c38cab9c333455e022e5c585b4f4a70
ballsona0925@naver.com <ballsona0925@naver.com>
2023-11-24 23:22:15 +0900

    PUR-58 feat: add dummyData

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -1,5 +1,11 @@
 import { UserResponses } from '@/constants/types';
 
+import { CultureContentInfoType } from './constants/types/CultureContentTypes';
+import {
+  OpinionDetailInfoType,
+  OpinionVoteInfoType,
+} from './constants/types/OpinionTypes';
+
 // 마이페이지 접속 시
 export const profileDummyData: UserResponses['info'] = {
   email: 'gwangin1999@naver.com',
@@ -10,105 +16,62 @@ export const profileDummyData: UserResponses['info'] = {
   profileUrl: '',
 };
 
-// 성동구 컨텐츠
-export const contentData = {
-  title: '국악노래(창) 배우기',
-  thumbnail: '/sample.jpg',
-  type: '교육 강좌',
-  startDate: '2023-11-03',
-  endDate: '2023-11-06',
-  recruitStartDate: '2023-11-03',
-  recruitEndDate: '2023-11-06',
+// 성동구 문화 컨텐츠
+export const cultureContentData = {
+  title: '12월 유아가족교육 <청계천에서 들려주는 자연생태동화> 모집 안내',
+  thumbnail:
+    'https://yeyak.seoul.go.kr/cmsdata/web_upload/svc/20231117/1700198515307MQ6Z5DV1OYB19K17R4S6EG1EH.png',
+  category: '교육/강좌',
+  state: '접수중',
+  description: '국악노래 배우기 컨텐츠에 대한 상세 설명입니다',
+  paymentMethod: '무료',
+  placeName: '청계천박물관(성동구)',
+  target: '가족(6~8세 및 동반가족_20가족)',
+  serviceUrl:
+    'https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S231117142107676002',
+  latitude: '127.0350413529804',
+  longitude: '37.571266444339074',
+  serviceStartDateTime: new Date('2023-11-20 00:00:00'),
+  serviceEndDateTime: new Date('2023-12-08 00:00:00'),
+  registerStartDateTime: new Date('2023-09-01 09:00:00'),
+  registerEndDateTime: new Date('2023-11-29 17:00:00'),
 };
 
-export const contentsData = Array(6)
+export const cultureContentListData: CultureContentInfoType[] = Array(6)
   .fill(0)
-  .map((_, i) => ({ ...contentData, id: i + 1 }));
+  .map((_, i) => ({
+    ...cultureContentData,
+    id: i + 1,
+    culturalEventId: i + 1,
+  }));
 
 // 시민 제안
-export const opinionData = {
+export const opinionVoteData: Omit<OpinionVoteInfoType, 'id'> = {
   status: 'vote',
-  title: '다양한 세대가 함께하는 애니메이션 모음전',
-  thumbnail: '/sample.jpg',
-  voteStartDate: '2023-11-03',
-  voteEndDate: '2023-11-06',
+  title: '다양한 세대가 함께하는 애니메이션 모음전을 진행하는 건 어떨까요?',
+  thumbnail: '/sample.png',
+  description:
+    '10대부터 20대, 30대까지 다양한 연령대의 사람들이 모두 즐길 수 있도록, 추억의 애니메이션에 함께 빠져보실 분 있나요? 10대부터 20대, 30대까지 다양한 연령대의 사람들이 모두 즐길 수 있도록, 추억의 애니메이션에 함께 빠져보실 분 있나요?',
+  voteStartDate: new Date('2023-11-03'),
+  voteEndDate: new Date('2023-11-06'),
+  voteData: '',
 };
 
-export const opinionsData = Array(6)
+export const opinionVoteListData: OpinionVoteInfoType[] = Array(6)
   .fill(0)
-  .map((_, i) => ({ ...opinionData, id: i + 1 }));
+  .map((_, i) => ({ ...opinionVoteData, id: i + 1 }));
 
-// 제안 리스트 조회 시
-// export const opinionData: ProjectResponses['main'] = {
-//  projectPartInformation: {
-//    id: 1,
-//    title: '우리 봄에 감자 농장 체험해요!',
-//    likeCount: 10,
-//    discussionStartDate: '2023-10-27',
-//    discussionEndDate: '2023-11-02',
-//    projectStartDate: '2023-11-03',
-//    projectEndDate: '2023-11-06',
-//    recruits: 0,
-//    totalRecruits: 10,
-//    facilityAddress: {
-//      city: '고양시',
-//      county: '대화역',
-//      district: '어딘가',
-//      jibun: '',
-//      detail: '',
-//      longitude: '',
-//      latitude: '',
-//    },
-//    ownerName: 'owner',
-//  },
-//  projectCategory: 'FARMING_HEALING',
-//  thumbnailFileRes: {
-//    projectFileUrl: '',
-//    projectFileType: 'THUMBNAIL',
-//  },
-// };
+export const opinionDetailData: Omit<OpinionDetailInfoType, 'id'> = {
+  status: 'finished',
+  title: '다양한 세대가 함께하는 애니메이션 모음전을 진행하는 건 어떨까요?',
+  thumbnail: '/sample.png',
+  description:
+    '10대부터 20대, 30대까지 다양한 연령대의 사람들이 모두 즐길 수 있도록, 추억의 애니메이션에 함께 빠져보실 분 있나요? 10대부터 20대, 30대까지 다양한 연령대의 사람들이 모두 즐길 수 있도록, 추억의 애니메이션에 함께 빠져보실 분 있나요?',
+  voteStartDate: new Date('2023-11-03'),
+  voteEndDate: new Date('2023-11-06'),
+  contentData: '',
+};
 
-// export const opinionsDummydata = Array(6)
-//  .fill(0)
-//  .map((v, i) => ({ ...opinionData, id: i }));
-
-/// / 제안 상세 데이터 조회 시
-// export const opinionContentDummyData: ProjectResponses['detail'] = {
-//  projectInformation: {
-//    title: '성동구 시민과 함께하는 추억의 애니메이션 모음전',
-//    likeCount: 10,
-//    discussionStartDate: '2023-10-27',
-//    discussionEndDate: '2023-11-02',
-//    projectStartDate: '2023-11-03',
-//    projectEndDate: '2023-11-06',
-//    recruits: 10,
-//    totalRecruits: 100,
-//    facilityAddress: {
-//      city: '고양시',
-//      county: '대화역',
-//      district: '어딘가',
-//      jibun: '',
-//      detail: '',
-//      longitude: '126.570667',
-//      latitude: '33.450701',
-//    },
-//    introduction:
-//      '10대부터 20대, 30대까지 다양한 연령대의 사람들이 모두 즐길 수 있도록, 추억의 애니메이션에 함께 빠져보실 분 있나요?',
-//    content: 'content',
-//    minAge: -1,
-//    maxAge: -1,
-//    guide: '',
-//    notice: '',
-//    ownerName: 'owner',
-//  },
-//  projectCategory: 'FARMING_HEALING',
-//  projectStatus: 'string',
-//  projectPayment: '10000원',
-//  projectFiles: [
-//    { projectFileType: 'THUMBNAIL', projectFileUrl: '' },
-//    {
-//      projectFileType: 'COMMON',
-//      projectFileUrl: '',
-//    },
-//  ],
-// };
+export const opinionDetailListData: OpinionDetailInfoType[] = Array(6)
+  .fill(0)
+  .map((_, i) => ({ ...opinionDetailData, id: i + 1 }));
