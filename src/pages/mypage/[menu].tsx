3df7d9ece6dbe9f5fce88fa3a555374134b32338
ballsona0925@naver.com <ballsona0925@naver.com>
2023-11-25 16:55:05 +0900

    PUR-62 fix: fix mypage type

--- src/pages/mypage/[menu].tsx
+++ src/pages/mypage/[menu].tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 import MypageDetailTemplate from '@/components/template/MypageDetailTemplate';
-import { MYPAGE_NAV_ITEM_NAME } from '@/constants/navigation';
+import { MyPAGE_NAV_ITEM_NAME as NavType } from '@/constants/navigation';
 
 /** 마이 페이지 세부 컨텐츠 페이지 (문화시민증, 뱃지목록, 활동목록, 내정보관리) */
 const MyPageDetail: NextPage = () => {
   const router = useRouter();
-  const menu = router.query.menu as MYPAGE_NAV_ITEM_NAME;
+  const menu = router.query.menu as NavType;
 
   return <MypageDetailTemplate menu={menu} />;
 };
