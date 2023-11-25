86726a95199d1a5f930adb63353bff75779d8425
RookieAND <gwangin1999@naver.com>
2023-11-25 17:12:59 +0900

    feat: 로그인, 회원가입 페이지 UI 구축 및 폼 적용

--- src/components/template/RegisterTemplate/RegisterTemplate.tsx
+++ src/components/template/RegisterTemplate/RegisterTemplate.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 
 import {
   FormProvider,
@@ -11,13 +12,25 @@ import { ApiErrorInstance } from '@/apis/API';
 import { AuthRepository } from '@/apis/auth';
 import Button from '@/components/common/Button';
 import Text from '@/components/common/Text';
-import PersonalDataForm from '@/components/domain/Register/PersonalDataForm';
+import AccountForm from '@/components/domain/Auth/AccountForm';
+import PersonalDataForm from '@/components/domain/Auth/PersonalDataForm';
+import REGISTER_FALLBACK from '@/constants/fallback/register';
 import { COLORS } from '@/constants/styles';
 import { type AuthFormType } from '@/constants/types';
-import { type RegisterProps } from '@/pages/auth/register';
 import ValidationUtil from '@/utils/validation';
 
 import * as style from './RegisterTemplate.style';
+
+const RegisterStepHeader = [
+  {
+    title: '회원가입',
+    subtitle: '사용할 아이디와 비밀번호를 입력해주세요.',
+  },
+  {
+    title: '환영해요 👋',
+    subtitle: '당신의 몇 가지 정보가 궁금해요!',
+  },
+];
 
 const today = new Date();
 const [currentYear, currentMonth, currentDay] = [
@@ -26,42 +39,68 @@ const [currentYear, currentMonth, currentDay] = [
   today.getUTCDate(),
 ];
 
-const RegisterTemplate = ({ socialType, socialEmail }: RegisterProps) => {
+const RegisterTemplate = () => {
   const router = useRouter();
   const formMethods = useForm<AuthFormType['register']>({
     defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
       name: '',
       birthday: [currentYear, currentMonth, currentDay],
       gender: 'MALE',
+      currentStep: 0,
     },
   });
   const {
     control,
+    setValue,
     setError,
     formState: { errors },
     handleSubmit,
   } = formMethods;
 
-  const [name, birthday] = useWatch({
-    control,
-    name: ['name', 'birthday'],
-  });
+  const [name, email, password, confirmPassword, birthday, currentStep] =
+    useWatch({
+      control,
+      name: [
+        'name',
+        'email',
+        'password',
+        'confirmPassword',
+        'birthday',
+        'currentStep',
+      ],
+    });
 
-  const shouldCheckCurrentStep = !!(
-    ValidationUtil.validateName(name) &&
-    ValidationUtil.validateBirthDay(birthday)
-  );
+  console.log({name, email, password, confirmPassword, birthday, currentStep})
+
+  const { title, subtitle } = RegisterStepHeader[currentStep];
+
+  // 다음 스텝으로 넘어가기 위한 조건을 충족했는지를 판별하는 함수 shouldCheckCurrentStep
+  const shouldCheckCurrentStep = useCallback(() => {
+    switch (currentStep) {
+      case 0:
+        return !!(
+          ValidationUtil.validateEmail(email) &&
+          password &&
+          password === confirmPassword
+        );
+      case 1:
+        return !!(
+          ValidationUtil.validateName(name) &&
+          ValidationUtil.validateBirthDay(birthday)
+        );
+      default:
+        return false;
+    }
+  }, [currentStep, email, password, confirmPassword, name, birthday]);
 
   const register: SubmitHandler<AuthFormType['register']> = async (
     submittedData,
   ) => {
     try {
-      // 이후 최종적으로 유저의 정보를 인계하여 회원가입 처리를 완료시킨다.
-      await AuthRepository.registerAsync({
-        ...submittedData,
-        email: socialEmail,
-        socialType,
-      });
+      await AuthRepository.registerAsync(submittedData);
       router.replace('/auth/login');
     } catch (error) {
       if (error instanceof ApiErrorInstance) {
@@ -73,20 +112,52 @@ const RegisterTemplate = ({ socialType, socialEmail }: RegisterProps) => {
     }
   };
 
+  const handleCurrentStep = () => {
+    switch (currentStep) {
+      case 0: {
+        if (!ValidationUtil.validatePassword(password)) {
+          setError('root', { message: REGISTER_FALLBACK.INVALID_PASSWORD });
+          return;
+        }
+        if (confirmPassword !== password) {
+          setError('root', { message: REGISTER_FALLBACK.NOT_MATCH_PASSWORDS });
+          return;
+        }
+        setValue('currentStep', currentStep + 1);
+        break;
+      }
+      case 1: {
+        if (!ValidationUtil.validateName(name)) {
+          setError('root', { message: REGISTER_FALLBACK.INVALID_NAME });
+          return;
+        }
+        if (!ValidationUtil.validateBirthDay(birthday)) {
+          setError('root', { message: REGISTER_FALLBACK.INVALID_BIRTHDAY });
+          return;
+        }
+        handleSubmit(register)();
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FormProvider {...formMethods}>
       <style.Wrapper>
         <style.Header>
           <Text color={COLORS.grayscale.dark} fontStyleName="subtitle1">
-            환영해요 👋
+            {title}
           </Text>
-          <Text color={COLORS.grayscale.gray500} fontStyleName="body1R">
-            당신의 몇 가지 정보가 궁금해요!
+          <Text color={COLORS.grayscale.gray500} fontStyleName="body2R">
+            {subtitle}
           </Text>
         </style.Header>
         <style.VisibleSection>
-          <style.Section>
+          <style.Section currentRegisterStep={currentStep}>
+            <AccountForm />
             <PersonalDataForm />
           </style.Section>
         </style.VisibleSection>
@@ -96,12 +167,12 @@ const RegisterTemplate = ({ socialType, socialEmail }: RegisterProps) => {
           ) : null}
           <Button
             themeColor={
-              shouldCheckCurrentStep
+              shouldCheckCurrentStep()
                 ? COLORS.primary.default
                 : COLORS.grayscale.gray400
             }
             isFilled
-            onClick={handleSubmit(register)}
+            onClick={handleCurrentStep}
             sizeType="large"
             className="confirm-button"
           >
