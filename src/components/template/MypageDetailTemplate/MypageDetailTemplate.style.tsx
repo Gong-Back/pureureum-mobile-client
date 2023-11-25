94dba3578d3ebc4ce69db0a0a3ed797cfd9888a7
ballsona0925@naver.com <ballsona0925@naver.com>
2023-11-25 16:53:15 +0900

    PUR-62 feat: add template for mypage

--- src/components/template/MypageDetailTemplate/MypageDetailTemplate.style.tsx
+++ src/components/template/MypageDetailTemplate/MypageDetailTemplate.style.tsx
@@ -1,5 +1,39 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  padding: 100px 60px 60px;
+  padding: 100px 30px;
+
+  .card {
+    position: relative;
+    min-height: calc(100vh - 60px);
+  }
+
+  .citizen-card {
+    position: absolute;
+    top: 630px;
+    left: 0;
+    transform-origin: top left;
+    transform: rotate(270deg);
+  }
+
+  .activity {
+    padding: 0px 20px;
+  }
+
+  .myinfo {
+    margin-top: 50px;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+
+export const ContentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
--- src/components/template/MypageTemplate/MypageTemplate.tsx
+++ src/components/template/MypageTemplate/MypageTemplate.tsx
@@ -1,15 +1,41 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import LeftIconSvg from '@/assets/icons/leftIcon.svg';
+import Text from '@/components/common/Text';
+import {
+  MypageNavInfo as info,
+  MyPAGE_NAV_ITEM_NAME as navItem,
+} from '@/constants/navigation';
 
 import * as styles from './MypageTemplate.style';
 
-const MypageTemplate = () => (
-  <styles.Wrapper>
-    <h3>성동구 문화 시민, 이공백</h3>
-    <Link href="/mypage/card">문화 시민증</Link>
-    <Link href="/mypage/badge">뱃지 목록</Link>
-    <Link href="/mypage/activity">활동 목록</Link>
-    <Link href="/mypage/myinfo">내 정보 관리</Link>
-  </styles.Wrapper>
-);
+const MypageTemplate = () => {
+  const router = useRouter();
+  const title = '이공백';
+  return (
+    <styles.Wrapper>
+      <styles.ProfileWrap>
+        <styles.ProfileImg>
+          <Image src="/sample.png" layout="fill" />
+        </styles.ProfileImg>
+        <styles.Title>
+          <Text fontStyleName="subtitle2R">
+            문화 시민, <span>{title}</span>
+          </Text>
+          <Text fontStyleName="body2R">성동구 피우다와 함께한지 42일째</Text>
+        </styles.Title>
+      </styles.ProfileWrap>
+      <styles.MenuListWrap>
+        {(Object.keys(info) as navItem[]).map((menu) => (
+          <styles.Menu key={menu} onClick={() => router.push(info[menu].path)}>
+            <Text fontStyleName="subtitle2B">{info[menu].text}</Text>
+            <LeftIconSvg width="28" height="28" />
+          </styles.Menu>
+        ))}
+      </styles.MenuListWrap>
+    </styles.Wrapper>
+  );
+};
 
 export default MypageTemplate;
