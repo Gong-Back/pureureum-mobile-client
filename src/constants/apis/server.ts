6f19da910560177e0cf04d83597036f602093498
ballsona0925@naver.com <ballsona0925@naver.com>
2023-11-27 01:00:19 +0900

    PUR-70 fix: update next server url to promotion

--- src/constants/apis/server.ts
+++ src/constants/apis/server.ts
@@ -2,6 +2,6 @@ export const API_URL = 'https://api.pureureum.site/api/v1' as const;
 
 export const NEXT_SERVER_URL = {
   development: 'http://localhost:3000',
-  production: 'https://dev.pureureum.site/',
-  test: 'https://dev.pureureum.site/',
+  production: 'https://pureureum.site/',
+  test: 'https://pureureum.site/',
 } as const;
