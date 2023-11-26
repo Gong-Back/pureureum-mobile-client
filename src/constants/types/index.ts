44d1215fe8d3d69c08307d525e3d7c07152e4699
ballsona0925@naver.com <ballsona0925@naver.com>
2023-11-26 23:44:32 +0900

    PUR-70 fix: modfy content types

--- src/constants/types/index.ts
+++ src/constants/types/index.ts
@@ -1,5 +1,6 @@
 export * from './apiTypes';
 export * from './authTypes';
+export * from './contentTypes';
 export * from './userTypes';
 export * from './OpinionTypes';
 export * from './projectTypes';
